91d047f730107bfff68319d186d73674edba01e4
Xeletron <xeletron1@gmail.com>
2019-08-06 21:31:14 +0200

    first rewrite

--- backend/server.js
+++ backend/server.js
@@ -6,42 +6,18 @@ const ext = require('commander');
 const jsonwebtoken = require('jsonwebtoken');
 const request = require('request');
 
-// The developer rig uses self-signed certificates.  Node doesn't accept them
-// by default.  Do not use this in production.
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-// Use verbose logging during development.  Set this to false for production.
-const verboseLogging = true;
-const verboseLog = verboseLogging ? console.log.bind(console) : () => { };
-
-// Service state variables
 const serverTokenDurationSec = 30;          // our tokens for pubsub expire after 30 seconds
 const userCooldownMs = 1000;                // maximum input rate per user to prevent bot abuse
 const userCooldownClearIntervalMs = 60000;  // interval to reset our tracking object
 const channelCooldownMs = 1000;             // maximum broadcast rate per channel
 const bearerPrefix = 'Bearer ';             // HTTP authorization headers have this prefix
-const cooldownData = {};
-const channelCooldowns = {};                // rate limit compliance
-let userCooldowns = {};                     // spam prevention
-let ingame = true;
-
-const STRINGS = {
-  secretEnv: usingValue('secret'),
-  clientIdEnv: usingValue('client-id'),
-  ownerIdEnv: usingValue('owner-id'),
-  serverStarted: 'Server running at %s',
-  secretMissing: missingValue('secret', 'EXT_SECRET'),
-  clientIdMissing: missingValue('client ID', 'EXT_CLIENT_ID'),
-  ownerIdMissing: missingValue('owner ID', 'EXT_OWNER_ID'),
-  messageSendError: 'Error sending message to channel %s: %s',
-  pubsubResponse: 'Message to c:%s returned %s',
-  executingCommand: 'Executing command for c:%s on behalf of u:%s',
-  broadcast: 'Broadcasting %s for c:%s',
-  send: 'Sending information %s to c:%s',
-  cooldown: 'Please wait before clicking again',
-  invalidAuthHeader: 'Invalid authorization header',
-  invalidJwt: 'Invalid JWT',
-};
+const userHeader = { 'Client-ID': 'js597m7tbf5l0g5vzcqgi3v8t7t5y2' };
+let userCooldowns = {};    
+let commandData = {};
+let commandStack = [];
+let gameData = {type: 0};
 
 ext.
   version(require('../package.json').version).
@@ -55,7 +31,7 @@ const secret = Buffer.from(getOption('secret', 'EXT_SECRET'), 'base64');
 const clientId = getOption('clientId', 'EXT_CLIENT_ID');
 
 const serverOptions = {
-  host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+  host: process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || 'localhost',
   port: process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
   routes: {
     cors: {
@@ -63,10 +39,10 @@ const serverOptions = {
     },
   },
 };
+
 const serverPathRoot = path.resolve(__dirname, '..', 'conf', 'server');
 if (fs.existsSync(serverPathRoot + '.crt') && fs.existsSync(serverPathRoot + '.key')) {
   serverOptions.tls = {
-    // If you need a certificate, execute "npm run cert".
     cert: fs.readFileSync(serverPathRoot + '.crt'),
     key: fs.readFileSync(serverPathRoot + '.key'),
   };
@@ -74,66 +50,44 @@ if (fs.existsSync(serverPathRoot + '.crt') && fs.existsSync(serverPathRoot + '.k
 const server = new Hapi.Server(serverOptions);
 
 (async () => {
+  
+  loadCommandData();
+  createRoute('POST', '/execute', executeHandler);
+  createRoute('GET', '/retrieve', clientHandler);
 
-  server.route({
-    method: 'POST',
-    path: '/execute',
-    handler: commandHandler,
-  });
-
-  server.route({
-    method: 'POST',
-    path: '/setGameInfo',
-    handler: setGameInfo,
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/cooldowns',
-    handler: QueryHandler,
-  });
-
-  server.route({
-    method: 'GET',
-    path: '/commandStack',
-    handler: retrieveStack,
-  });
-
-  // Start the server.
   await server.start();
-  console.log(STRINGS.serverStarted, server.info.uri);
+  console.log("Server running at %s", server.info.uri);
 
-  // Periodically clear cool-down tracking to prevent unbounded growth due to
-  // per-session logged-out user tokens.
   setInterval(() => { userCooldowns = {}; }, userCooldownClearIntervalMs);
+
 })();
 
-function usingValue(name) {
-  return `Using environment variable for ${name}`;
+function createRoute(method, path, handler) {
+  server.route({
+    method: method,
+    path: path,
+    handler: handler,
+  });
 }
 
-function missingValue(name, variable) {
-  const option = name.charAt(0);
-  return `Extension ${name} required.\nUse argument "-${option} <${name}>" or environment variable "${variable}".`;
+function loadCommandData() {
+  let raw = fs.readFileSync('../config.json');
+  commandData = JSON.parse(raw);
 }
 
-// Get options from the command line or the environment.
 function getOption(optionName, environmentName) {
   const option = (() => {
     if (ext[optionName]) {
       return ext[optionName];
     } else if (process.env[environmentName]) {
-      console.log(STRINGS[optionName + 'Env']);
       return process.env[environmentName];
     }
-    console.log(STRINGS[optionName + 'Missing']);
     process.exit(1);
   })();
   console.log(`Using "${option}" for ${optionName}`);
   return option;
 }
 
-// Verify the header and the enclosed JWT.
 function verifyAndDecode(header) {
   if (header.startsWith(bearerPrefix)) {
     try {
@@ -141,108 +95,12 @@ function verifyAndDecode(header) {
       return jsonwebtoken.verify(token, secret, { algorithms: ['HS256'] });
     }
     catch (ex) {
-      throw Boom.unauthorized(STRINGS.invalidJwt);
+      throw Boom.unauthorized("Invalid JWT");
     }
   }
-  throw Boom.unauthorized(STRINGS.invalidAuthHeader);
+  throw Boom.unauthorized("Invalid authorization header");
 }
 
-function commandHandler(req) {
-  if (ingame == "1"){
-      // Verify all requests.
-      const payload = verifyAndDecode(req.headers.authorization);
-      const { channel_id: channelId, opaque_user_id: opaqueUserId, user_id: userId } = payload;
-
-      let data = req.payload;
-      let commandData = {command: data.command, time: Date.now() / 1000 + 20, user: data.user};
-      // Bot abuse prevention:  don't allow a user to spam the button.
-      if (userIsInCooldown(opaqueUserId)) {
-        throw Boom.tooManyRequests(STRINGS.cooldown);
-      }
-      verboseLog(STRINGS.executingCommand, channelId, opaqueUserId);
-
-
-      if (!cooldownData)
-      cooldownData = {};
-      
-      cooldownData[data.command] = commandData;
-
-
-      attemptBroadcast(channelId, data.command);
-
-      return cooldownData;
-    }
-    return false;
-}
-function setGameInfo(req) {
-  ingame = req.payload.ingame;
-  verboseLog(req.payload);
-  const commandData = cooldownData || {};
-  return commandData;
-}
-function QueryHandler(req) {
-  // Verify all requests.
-  const payload = verifyAndDecode(req.headers.authorization);
-
-  const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
-  const commandData = cooldownData || {};
-  verboseLog(STRINGS.send, commandData, opaqueUserId);
-  return commandData;
-}
-function retrieveStack(req) {
-  const { channel_id: channelId, opaque_user_id: opaqueUserId } = req;
-  const commandData = cooldownData || {};
-  verboseLog(STRINGS.send, commandData, opaqueUserId);
-  return commandData;
-}
-
-function attemptBroadcast(channelId, commandName) {
-  // Check the cool-down to determine if it's okay to send now.
-  const now = Date.now() / 1000;
-
-    sendBroadcast(channelId, commandName);
-
-}
-
-function sendBroadcast(channelId, commandName) {
-  // Set the HTTP headers required by the Twitch API.
-  const headers = {
-    'Client-ID': clientId,
-    'Content-Type': 'application/json',
-    'Authorization': bearerPrefix + makeServerToken(channelId),
-  };
-
-  // Create the POST body for the Twitch API request.
-  const commandData = JSON.stringify(cooldownData[commandName]) || "{time: 0, command: commandName, user: twitch}";
-  const body = JSON.stringify({
-    content_type: 'application/json',
-    message: commandData,
-    targets: ['broadcast'],
-  });
-
-  // Send the broadcast request to the Twitch API.
-  verboseLog(STRINGS.broadcast, commandData, channelId);
-  request(
-    `https://api.twitch.tv/extensions/message/${channelId}`,
-    {
-      method: 'POST',
-      headers,
-      body,
-    }
-    , (err, res) => {
-      if (err) {
-        console.log(STRINGS.messageSendError, channelId, err);
-      } else {
-        verboseLog(STRINGS.pubsubResponse, channelId, res.statusCode);
-      }
-    });
-}
-
-function getUserInfo(data) {
-  verboseLog("name: ", data.display_name);
-}
-
-// Create and return a JWT for use by this service.
 function makeServerToken(channelId) {
   const payload = {
     exp: Math.floor(Date.now() / 1000) + serverTokenDurationSec,
@@ -263,8 +121,99 @@ function userIsInCooldown(opaqueUserId) {
   if (cooldown && cooldown > now) {
     return true;
   }
-
   // Voting extensions must also track per-user votes to prevent skew.
   userCooldowns[opaqueUserId] = now + userCooldownMs;
   return false;
+}
+
+function isCommandOnCooldown(command, client, time){
+  let data = commandData[command]
+  
+  if (data.time && data.client == client && data.time + data.user > time)
+    return true;
+  else if(data.time && data.client != client && data.time + data.global > time)
+    return true;
+
+  return false;
+}
+
+function executeHandler(req) {
+  const payload = verifyAndDecode(req.headers.authorization);
+  const { channel_id: channelId, opaque_user_id: opaqueUserId } = payload;
+
+  let payloadData = req.payload;
+  let command = payloadData.command
+  let time = Date.now() / 1000;
+  let client = payloadData.uid;
+
+  // Bot abuse prevention:  don't allow a user to spam the button.
+  if (userIsInCooldown(opaqueUserId)) {
+    throw Boom.tooManyRequests("Too many requests, please wait");
+  }
+  // Dont execute if command is on cooldown
+  if(isCommandOnCooldown(command, client, time))
+    throw Boom.notAcceptable("Command is still on cooldown");
+
+  console.log(`Executing ${command}`);
+
+  commandData[command].time = time;
+  commandData[command].client = client;
+
+  let message = {command: command, time: time, client: client};
+
+  sendBroadcast(channelId, message);
+  //commandStack.push(message);
+  return message;
+}
+
+function sendBroadcast(channelId, message) {
+  // Set the HTTP headers required by the Twitch API.
+  const headers = {
+    'Client-ID': clientId,
+    'Content-Type': 'application/json',
+    'Authorization': bearerPrefix + makeServerToken(channelId),
+  };
+
+  // Create the POST body for the Twitch API request.
+  const data = JSON.stringify(message)
+  const body = JSON.stringify({
+    content_type: 'application/json',
+    message: data,
+    targets: ['broadcast'],
+  });
+
+  // Send the broadcast request to the Twitch API.
+  console.log(`Broadcasting ${data}`);
+  request(`https://api.twitch.tv/extensions/message/${channelId}`,
+    {method: 'POST',
+      headers,
+      body,
+    }
+    , (err, res) => {
+      if (err) {
+        console.log("Error sending message");
+      } else {
+        console.log(`Message returned ${res.statusCode}`);
+      }
+  });
+}
+
+function clientHandler(req) {
+  const payload = verifyAndDecode(req.headers.authorization);
+  const { user_id: userID } = payload;
+  console.log(`Sending data to ${userID}`);
+  let reply = {commandData: commandData, gameData: gameData};
+  return reply;
+}
+
+function getUserName(uid){
+  var username = "";
+  request({url:`https://api.twitch.tv/helix/users?id=${uid}`, method: 'GET', headers: userHeader}, function(err, res, body) {
+    if(!err){
+      let parsed = JSON.parse(body);
+      let [user] = parsed.data;
+      username = user.display_name
+    }
+  });
+  return username;
 }
